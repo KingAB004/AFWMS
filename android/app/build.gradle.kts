ece4b8581f0e52ddd1ff8f86686b219db0e4d353
josh <qjgmalonzo@tip.edu.ph>
2026-03-20 17:45:04 +0800

    beta test on android app

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -16,6 +16,8 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
+        // Enable desugaring for libraries that need newer Java APIs.
+        isCoreLibraryDesugaringEnabled = true
     }
 
     kotlinOptions {
@@ -44,4 +46,8 @@ android {
 
 flutter {
     source = "../.."
+}
+
+dependencies {
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.4")
 }
